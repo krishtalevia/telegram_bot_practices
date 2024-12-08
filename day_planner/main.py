b0989e0837603bc3e63989ad9cf4eb509b68dcfe
krishtalevia <krishtalev.ilya@gmail.com>
2024-12-08 21:27:42 +0300

    add remove/clear/show tasks to day planner

--- day_planner/main.py
+++ day_planner/main.py
@@ -1,15 +1,17 @@
 import asyncio
-
 from aiogram import Bot, Dispatcher, types
 from aiogram.filters.command import Command, CommandObject
+from config import TOKEN
+import time
 
-import datetime
-
-token = '7581111496:AAFXdQGFs6dORM0FbNyyLMp33HTman8q_X8'
-
-bot = Bot(token = token)
-
+bot = Bot(token = TOKEN)
 dp = Dispatcher()
+
+user_tasks = {}
+
+def add_user_task(user_id: int) -> None:
+    if user_id not in user_tasks.keys():
+        user_tasks[user_id] = {}
 
 @dp.message(Command('start'))
 async def start_handler(message: types.Message) -> None:
@@ -17,30 +19,69 @@ async def start_handler(message: types.Message) -> None:
 
 @dp.message(Command('add_task'))
 async def add_task_handler(message: types.Message, command: CommandObject) -> None:
+    add_user_task(message.chat.id)
+
+    command_description = '/add task принимает два параметра: <время> <описание> — добавляет задачу (например, /add_task 12:00 Купить продукты).'
+
     if command.args is None:
-        await message.answer(
-            '/add task принимает два параметра: <время> <описание> ' +
-            '— добавляет задачу (например, /add_task 12:00 Купить продукты).'
-            )
+        await message.answer(command_description)
         return
     
-    if len(command.args.split()) != 2:
-        await message.answer(
-            '/add task принимает два параметра: <время> <описание> ' +
-            '— добавляет задачу (например, /add_task 12:00 Купить продукты).'
-            )
+    args = command.args.split()
+    if len(args) < 2:
+        await message.answer(command_description)
         return
     
-    time = str(command.args[0:5])
-    task = str(command.args[6::])
+    time_str = args[0]
+    task = ' '.join(args[1:])
 
     try:
-        time = time.strptime(time, '%H:%M')
+        time.strptime(time_str, '%H:%M')
     except ValueError:
         await message.answer('Команда должна содержать в себе время в формате чч:мм.')
+        return
+
+    user_tasks[message.chat.id][time_str] = task
+    await message.answer(f'Задача "{task}" добавлена на {time_str}')
+
+@dp.message(Command('show_tasks'))
+async def show_tasks_handler(message: types.Message) -> None:
+    add_user_task(message.chat.id)
+
+    tasks = user_tasks[message.chat.id]
+    if len(tasks) == 0:
+        await message.reply(f'У вас нет задач.')
+        return
+    
+    tasks_list = []
+    for time, task in tasks.items():
+        tasks_list.append(f'{time}: {task}')
+
+    await message.answer('\n'.join(tasks_list))
+
+@dp.message(Command('remove_task'))
+async def remove_task_handler(message: types.Message, command: CommandObject) -> None:
+    add_user_task(message.chat.id)
+
+    if command.args is None:
+        await message.reply('Укажите время задачи, для ее удаления (чч:мм)')
+        return
+    
+    chosen_time = command.args
+    if chosen_time in user_tasks[message.chat.id]:
+        del user_tasks[message.chat.id][chosen_time]
+        await message.answer('Задача на {chosen_time} удалена.')
+    else:
+        await message.answer('Задачи на {chosen_time} не найдено.')
+
+@dp.message(Command('clear_tasks'))
+async def clear_tasks_handler(message: types.Message) -> None:
+    add_user_task(message.chat.id)
+    user_tasks[message.chat.id].clear()
+    await message.answer('Все задачи удалены.')
 
 async def main():
     await dp.start_polling(bot)
 
-if __name__ = '__main__':
+if __name__ == '__main__':
     asyncio.run(main())
